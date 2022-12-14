4f8acb70efeb2f6a9882213a7b0426f57fd065e2
cedricd <cedricdecruw@gmail.com>
2022-12-14 22:44:09 +0100

    basic workflow info gathering added + updated token generation

--- my-app/src/api/octokit.js
+++ my-app/src/api/octokit.js
@@ -1,15 +1,20 @@
 import { Octokit } from '@octokit/rest';
 //import env variables from .env file
 console.log(process.env.REACT_APP_HASHED_TOKEN);
-//remove the \" from the hashed token"
-const decoded_token_a = b64_to_utf8(process.env.REACT_APP_HASHED_TOKEN);
+const decoded_token_a = fromBinary(process.env.REACT_APP_HASHED_TOKEN);
+console.log(decoded_token_a);
 // Create a new Octokit instance that uses axios as its HTTP client
 const octokit = new Octokit({
     auth: decoded_token_a
 });
   
-function b64_to_utf8(str) {
-return decodeURIComponent(escape(window.atob(str)));
+function fromBinary(encoded) {
+    const binary = atob(encoded);
+    const bytes = new Uint8Array(binary.length);
+    for (let i = 0; i < bytes.length; i++) {
+      bytes[i] = binary.charCodeAt(i);
+    }
+    return String.fromCharCode(...new Uint16Array(bytes.buffer));
 }
 
 //async funtion that will return a promise
@@ -25,4 +30,15 @@ export async function getRepos(setLoading,setError,username) {
     }
     
     return [];
+}
+
+//async function that will return the workflows given a repo
+export async function getWorkflows(username,repo) {
+    //perform axios request to get workflows
+    //return the workflows
+    const request_workflows = await octokit.request('GET /repos/{owner}/{repo}/actions/workflows', {
+        owner: username,
+        repo: repo
+    });
+    return request_workflows.data;
 }
--- my-app/src/components/git_repos_table/git_repo_tables.js
+++ my-app/src/components/git_repos_table/git_repo_tables.js
@@ -3,26 +3,73 @@
 import React, { Component } from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
-import { getRepos } from '../../api/octokit';
+import { getRepos , getWorkflows } from '../../api/octokit';
 
 const GitReposTable = () => {
     const [repos, setRepos] = useState([]);
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
     const [search, setSearch] = useState('');
     const [searched, setSearched] = useState("");
     //function that will search for a user or org
     const searchUser = () => {
         getRepos(setLoading,setError,search).then((repos) => {
+
+            for (let i = 0; i < repos.length; i++) {
+                repos[i]['workflows_loaded'] = false; 
+            }
             setRepos(repos);
             setLoading(false);
             setSearched(search);
             setSearch('');
         }, (error) => {
+            console.log(error);
             setError(true);
             setLoading(false);
             setSearch('');
         });
+    }
+
+    //function here that will return a value of a cell in the table
+    const getWorkflowCell = (repo) => {
+
+        if (repo.workflows_loaded){
+            return repo.workflows.workflows.map((workflow) => {
+                return(
+                    <li>{workflow.name}=>{workflow.state}</li>
+                )
+            })
+        }
+        if (!repo.workflows_loaded){
+            //get the workflows for the repo
+            //return the workflows
+            setLoading(true);
+            getWorkflows(searched,repo.name).then((workflows) => {
+                //loop over repos and find the repo that matches the repo that was passed in
+                for(let i = 0; i < repos.length; i++){
+                    if (repos[i].id == repo.id){
+                        repos[i]['workflows_loaded'] = true;
+                        repos[i]['workflows'] = workflows;
+                    }
+                }
+                setLoading(false);
+                setRepos(repos);
+                console.log(repos);
+            }, (error) => {
+                console.log(error);
+                return(<>Error while fetching...</>)
+            }
+            );
+            return(<>Loading...</>)
+        }
+    }
+    if (loading && searched == '') {
+        return (
+            <div className='table_main_page'>
+                <input type="text" placeholder="Search for a user or org" onChange={e => setSearch(e.target.value)} value={search}/>
+                <button onClick={e => searchUser()}>search</button>
+            </div>
+        );
     }
 
     if (loading) {
@@ -36,21 +83,19 @@ const GitReposTable = () => {
             <div className='table_main_page'>
                 <input type="text" placeholder="Search for a user or org" onChange={e => setSearch(e.target.value)} value={search}/>
                 <button onClick={e => searchUser()}>search</button>
-
                 <h1>{searched} Github Repos</h1>
-
                 <table>
                     <thead>
                         <tr>
                             <th>Repo Name</th>
-                            <th>Repo URL</th>
+                            <th>workflows</th>
                         </tr>
                     </thead>
                     <tbody>
                         {repos.map((repo) => (
                             <tr key={repo.id}>
-                                <td>{repo.name}</td>
-                                <td><a href={repo.html_url} target="_blank">{repo.html_url}</a></td>
+                                <td><a href={repo.html_url} target="_blank">{repo.name}</a></td>
+                                <td>{getWorkflowCell(repo)}</td>
                             </tr>
                         ))}
                     </tbody>
